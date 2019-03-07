adbbe730072616e21170528b3a2915ca3657656b
Jackie Lealess <jlealess@gmail.com>
2019-03-07 17:01:03 -0500

    Getting data from API

--- src/app/modules/games/games.module.ts
+++ src/app/modules/games/games.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { gamesReducer } from './store/reducers/games.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { GamesEffects } from '../games/store/effects/games.effects';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     StoreModule.forFeature('games', gamesReducer),
+    EffectsModule.forFeature([GamesEffects])
   ]
 })
 export class GamesModule { }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { profileReducer } from './store/reducers/profile.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ProfileEffects } from '../profile/store/effects/profile.effects';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     StoreModule.forFeature('profile', profileReducer),
+    EffectsModule.forFeature([ProfileEffects])
   ]
 })
 export class ProfileModule { }
