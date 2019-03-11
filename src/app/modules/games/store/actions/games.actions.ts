027256243ac50a17d36ef021cfa7e7af62f9b037
Jackie Lealess <jlealess@gmail.com>
2019-03-11 17:01:55 -0400

    More cleanup in games module

--- src/app/modules/games/store/actions/games.actions.ts
+++ src/app/modules/games/store/actions/games.actions.ts
@@ -3,17 +3,11 @@ import { Game } from '../../../../models/game.models';
 
 export enum GamesActionTypes {
   SetGames = 'SET_GAMES',
-  SetPlatforms = 'SET_PLATFORMS',
   FetchGames = 'FETCH_GAMES',
-  FetchPlatforms = 'FETCH_PLATFORMS'
 }
 
 export class FetchGames implements Action {
   readonly type = GamesActionTypes.FetchGames;
-}
-
-export class FetchPlatforms implements Action {
-  readonly type = GamesActionTypes.FetchPlatforms;
 }
 
 export class SetGames implements Action {
